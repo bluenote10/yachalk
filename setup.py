3ba09a3e65c20546e197782a3ea8b9a96af58208
Fabian Keller <github.100.fkeller@spamgourmet.com>
2021-07-29 16:23:10 +0200

    moved marker file

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     setuptools.setup(
         package_data={
             "yachalk": ["py.typed"],
-        }
+        },
+        include_package_data=True,
     )
--- yachalk/__init__.py
+++ yachalk/__init__.py
@@ -2,7 +2,7 @@ from .chalk_instance import chalk, create_chalk
 from .chalk_factory import ChalkFactory
 from .types import ColorMode
 
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 __all__ = [
     "chalk",
